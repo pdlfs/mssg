f9e1aa3f5ba8e8168ff47944cabc86896bcde476
Qing Zheng <zhengq@cs.cmu.edu>
2018-08-20 12:14:16 -0400

    clean mssg's api

--- mssg.h
+++ mssg.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 UChicago Argonne, LLC
+ * Copyright (c) 2018 Carnegie Mellon University.
  *
  * See COPYRIGHT in top-level directory.
  */
@@ -17,8 +18,8 @@
 extern "C" {
 #endif
 
-struct mssg;                     /* forward decl */
-typedef struct mssg *mssg_t;
+struct mssg; /* forward decl */
+typedef struct mssg mssg_t;
 
 /**
  * mssg_init_mpi: init mssg using MPI!
@@ -27,14 +28,14 @@ typedef struct mssg *mssg_t;
  * @param comm the MPI communicator to use
  * @return pointer to our structure
  */
-mssg_t mssg_init_mpi(hg_class_t *hgcl, MPI_Comm comm);
+mssg_t* mssg_init_mpi(hg_class_t* hgcl, MPI_Comm comm);
 
 /**
  * mssg_finalize: free mssg state
  *
  * @param s the mssg to finalize and free
  */
-void mssg_finalize(mssg_t s);
+void mssg_finalize(mssg_t* s);
 
 /**
  * mssg_get_rank: get my rank
@@ -42,7 +43,7 @@ void mssg_finalize(mssg_t s);
  * @param s our mssg
  * @return our rank
  */
-int mssg_get_rank(const mssg_t s);
+int mssg_get_rank(const mssg_t* s);
 
 /**
  * mssg_get_count: get the number of participants (e.g. world size)
@@ -50,7 +51,7 @@ int mssg_get_rank(const mssg_t s);
  * @param s our mssg
  * @return world size
  */
-int mssg_get_count(const mssg_t s);
+int mssg_get_count(const mssg_t* s);
 
 /**
  * mssg_get_addr: get addres for group member of given rank
@@ -58,7 +59,7 @@ int mssg_get_count(const mssg_t s);
  * @param s our mssg
  * @return mercury address
  */
-hg_addr_t mssg_get_addr(const mssg_t s, int rank);
+hg_addr_t mssg_get_addr(const mssg_t* s, int rank);
 
 /**
  * mssg_get_addr_str: get string addr for given rank
@@ -66,7 +67,7 @@ hg_addr_t mssg_get_addr(const mssg_t s, int rank);
  * @param s our mssg
  * @return mercury address string
  */
-const char *mssg_get_addr_str(const mssg_t s, int rank);
+const char* mssg_get_addr_str(const mssg_t* s, int rank);
 
 /**
  * mssg_lookup: lookup all the addresses of interest in one big go
@@ -75,9 +76,7 @@ const char *mssg_get_addr_str(const mssg_t s, int rank);
  * @parma hgctx mercury context to use (XXX: init caches class, but not ctx)
  * @return success or error code
  */
-hg_return_t mssg_lookup(mssg_t s, hg_context_t *hgctx);
-
-
+hg_return_t mssg_lookup(mssg_t* s, hg_context_t* hgctx);
 
 #ifdef __cplusplus
 }
